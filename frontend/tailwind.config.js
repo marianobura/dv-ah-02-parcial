8d2d05af396da6a1cae967e46155e127aabee702
marianobura <marianoobura@gmail.com>
2024-11-10 20:26:24 -0300

    Se agregó un estilo a index.html y se agregó Tailwind al proyecto

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      gridTemplateRows: {
+        body: '200px 1fr 100px'
+      },
+    },
+  },
+  plugins: [],
+}
