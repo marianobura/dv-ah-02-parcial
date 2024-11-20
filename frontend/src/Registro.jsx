58babc1d52fad5e84c50e6c3b0317ec758cff9e7
marianobura <>
2024-11-20 09:59:49 -0300

    Agregado axios

--- frontend/src/Registro.jsx
+++ frontend/src/Registro.jsx
@@ -62,17 +62,17 @@ const Registro = () => {
                     <form className="space-y-4 md:space-y-6" onSubmit={ handleSubmit }>
                         <div>
                             <label htmlFor="nombre" className="block mb-2 text-sm font-medium text-gray-900">Nombre</label>
-                            <input type="nombre" name="nombre" id="nombre" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required="" onChange={handleChange} value={formData.nombre} />
+                            <input type="nombre" name="nombre" id="nombre" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Juan Pedro" required="" onChange={handleChange} value={formData.nombre} />
                         </div>
                         <div>
                             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
-                            <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required="" onChange={handleChange} value={formData.email} />
+                            <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="google@gmail.com" required="" onChange={handleChange} value={formData.email} />
                         </div>
                         <div>
                             <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
                             <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required="" onChange={handleChange} value={formData.password} />
                         </div>
-                        <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Create an account</button>
+                        <button type="submit" className="w-full text-white bg-blue-600 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">Crear cuenta</button>
                         <p className="text-sm font-light text-gray-500">
                             Already have an account? <a href="#" className="font-medium text-primary-600 hover:underline">Login here</a>
                         </p>
